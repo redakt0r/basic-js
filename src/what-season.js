49858ae25e871b3e1c8ecc706602af3360087d2e
Mikhail Koryakin <sdrr87@gmail.com>
2023-12-15 21:55:16 +0300

    feat: what-season (not exactly)

--- src/what-season.js
+++ src/what-season.js
@@ -11,8 +11,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+  if (date === undefined) return 'Unable to determine the time of year!';
+  if (!(date instanceof Date)) throw new Error('Invalid date!');
+  //if (isNaN(date.getTime())) throw new Error('Invalid date!');
+  //console.debug(date instanceof Date)
+  const m = date.getUTCMonth();
+  //console.debug(String(date))
+  if (m === 11 || m === 0 || m === 1) return 'winter';
+  if (m === 2 || m === 3 || m === 4) return 'spring';
+  if (m === 5 || m === 6 || m === 7) return 'summer';
+  if (m === 8 || m === 9 || m === 10) return 'fall';  
 }
 
 module.exports = {
