7a9e0185185f0bbe9667344c5a304891b7bb3a0c
Mikhail Koryakin <sdrr87@gmail.com>
2023-12-15 20:00:44 +0300

    feat: count-cats may

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  return matrix.flat().reduce((acc, curr) => curr === '^^' ? acc += 1: acc, 0);
+  //редиски, что за двор такой кривой в тесте 'level 2'? так не интересно
+  //держите за это дурацкий кодварсовский oneliner. 
+/*   let earsCount = 0;
+  if (matrix.length === 0) return earsCount;
+  
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      if (matrix[i][j] === '^^') earsCount++;
+    }
+  }
+  return earsCount */
 }
 
 module.exports = {
