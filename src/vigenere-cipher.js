cd4c7b32ea7c46480b0d9eaf923b943941548346
Mikhail Koryakin <sdrr87@gmail.com>
2023-12-16 18:50:52 +0300

    feat: vigenere-cipher (not exactly)

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect) {
+    this.isDirect = !isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    const upperMessage = message.toUpperCase();
+    const upperKey = key.toUpperCase();
+
+    const encryptedMessage = [];
+    let keyIndex = 0;
+    
+    for (let i = 0; i < upperMessage.length; i++) {
+      const char = upperMessage[i];
+      if (char <= 'Z' && char >= 'A') {
+        const offsetCharCode = char.charCodeAt(0) - 65;
+        const offsetKeyCode = upperKey[keyIndex].charCodeAt(0) - 65;
+        const charCode = (offsetCharCode + offsetKeyCode) % 26 + 65;
+        encryptedMessage.push(String.fromCharCode(charCode));
+        keyIndex = keyIndex === (upperKey.length - 1) ? 0 : keyIndex + 1;
+      } else encryptedMessage.push(char);
+    }
+    return this.isDirect ? encryptedMessage.join('') : encryptedMessage.reverse().join('');
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) throw new Error('Incorrect arguments!');
+    const upperMessage = encryptedMessage.toUpperCase();
+    const upperKey = key.toUpperCase();
+
+    const decryptedMessage = [];
+    let keyIndex = 0;
+    
+    for (let i = 0; i < upperMessage.length; i++) {
+      const char = upperMessage[i];
+      if (char <= 'Z' && char >= 'A') {
+        const offsetCharCode = char.charCodeAt(0) - 65;
+        const offsetKeyCode = upperKey[keyIndex].charCodeAt(0) - 65;
+        const charCode = (offsetCharCode - offsetKeyCode + 26) % 26 + 65;
+        decryptedMessage.push(String.fromCharCode(charCode));
+        keyIndex = keyIndex === (upperKey.length - 1) ? 0 : keyIndex + 1;
+      } else decryptedMessage.push(char);
+    }
+    return this.isDirect ? decryptedMessage.join('') : decryptedMessage.reverse().join('');
   }
 }
 
