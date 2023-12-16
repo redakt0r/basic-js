3e0c324a466e5e2b7fe00813c536adfc337d71b9
Mikhail Koryakin <sdrr87@gmail.com>
2023-12-16 14:36:59 +0300

    refactor: count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -15,9 +15,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function countCats(matrix) {
-  return matrix.flat().reduce((acc, curr) => curr === '^^' ? acc += 1: acc, 0);
+  let earsCount = 0;
+  if (matrix.length === 0) return earsCount;
+  
+  const rows = matrix.length;
+  for (let i = 0; i < rows; i++) {    
+    const cols = matrix[i].length;
+    for (let j = 0; j < cols; j++) {
+      if (matrix[i][j] === '^^') earsCount++;
+    }
+  }
+  return earsCount
+
   //редиски, что за двор такой кривой в тесте 'level 2'? так не интересно
   //держите за это дурацкий кодварсовский oneliner. 
+  
+  // return matrix.flat().reduce((acc, curr) => curr === '^^' ? acc += 1: acc, 0);
+  // извините, это я ограниченный человек (((
 /*   let earsCount = 0;
   if (matrix.length === 0) return earsCount;
   
